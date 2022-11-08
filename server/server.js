13cc99a574cce206b1227c18d1337cb99120606a
Shailyn Moore <m.shailyn@yahoo.com>
2022-11-07 22:32:17 -0800

    fix react routes

--- server/server.js
+++ server/server.js
@@ -27,16 +27,16 @@ app.use('/graphql', graphqlHTTP({
 //When you navigate to the root page, it would use the built react-app
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static(__dirname, '..', 'client', 'build'));
   app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.join(__dirname, "../client/build/index.html"));
   });
 }
 
 else {
-  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.use(express.static(path.join(__dirname, '../client/public')));
   app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+    res.sendFile(path.join(__dirname, "../client/public/index.html"));
   });
 }
 //app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
