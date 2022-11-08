328a97c92ce15031afcdd42ee593dd368d293d9d
Shailyn Moore <m.shailyn@yahoo.com>
2022-11-07 22:30:23 -0800

    fix routes

--- server/server.js
+++ server/server.js
@@ -25,11 +25,22 @@ app.use('/graphql', graphqlHTTP({
 
 //This will create a middleware.
 //When you navigate to the root page, it would use the built react-app
-app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
-// app.get('*', function (req, res) {
-//   const index = path.join(__dirname, '..', 'client', 'build', 'index.html');
-//   res.sendFile(index);
-// });
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
+
+else {
+  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
+}
+//app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
 
 
 //Server Running
